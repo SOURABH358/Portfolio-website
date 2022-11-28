6b3497515fff50cc1a4291bada5109094a639282
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-28 11:20:12 +0530

    added files

--- Home.js
+++ Home.js
@@ -1,12 +1,23 @@
-const floatNavbar = document.querySelector('#float__navbar')
-const arrow = document.getElementById('arrow');
-floatNavbar.addEventListener('click',(e)=>{
-    if(e.currentTarget!== e.target){
-        Array.from(e.currentTarget.children).map(el=>el.classList.remove('active'))
-        e.target.classList.add('active')
-    }
-})
+// const floatNavbar = document.querySelector('#float__navbar')
+// const arrow = document.getElementById('arrow');
+// floatNavbar.addEventListener('click',(e)=>{
+//     if(e.currentTarget!== e.target){
+//         Array.from(e.currentTarget.children).map(el=>el.classList.remove('active'))
+//         e.target.classList.add('active')
+//     }
+// })
 
-arrow.addEventListener('click',(e)=>{
-    floatNavbar.classList.toggle('show')
+// arrow.addEventListener('click',(e)=>{
+//     floatNavbar.classList.toggle('show')
+// })
+
+const close = document.getElementById('close')
+const hamburger = document.getElementById('hamburger')
+const navBar = document.getElementById('navbar__section')
+
+hamburger.addEventListener('click',()=>{
+    navBar.classList.add('show')
+})
+close.addEventListener('click',()=>{
+    navBar.classList.remove('show')
 })
