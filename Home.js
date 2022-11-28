df0e5267c0a51aae5d46883390847d3bd72c9293
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-28 12:35:44 +0530

    added files

--- Home.js
+++ Home.js
@@ -14,10 +14,25 @@
 const close = document.getElementById('close')
 const hamburger = document.getElementById('hamburger')
 const navBar = document.getElementById('navbar__section')
+const headerSection = document.getElementById('header__section')
+const educationSection = document.getElementById('education__section')
 
 hamburger.addEventListener('click',()=>{
     navBar.classList.add('show')
 })
 close.addEventListener('click',()=>{
     navBar.classList.remove('show')
+})
+
+window.addEventListener('scroll',()=>{
+    headerSection.classList.add('header__section__scroll')
+    if(scrollY===0){
+        headerSection.classList.remove('header__section__scroll')
+    }
+    if(educationSection.getBoundingClientRect().top<600){
+        educationSection.classList.add('slideDown');
+    }
+    else{
+        educationSection.classList.remove('slideDown');
+    }
 })
