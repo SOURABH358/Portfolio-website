8c56732999cb2531f421e8852be93c0fcedbced8
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-10 16:22:03 +0530

    added files

--- data.js
+++
@@ -1,9 +0,0 @@
-const projects = [{
-    title: "Netflix Clone",
-    description: "A fully responsive Netflix clone, created using ReactJs. For navigation inside the website I have used React router. The entire website is styled using CSS and Material UI. The project is hosted on netlify and developed using create react app.",
-    techstack: ["ReactJs, CSS, Material UI, React Router"],
-    image: "./assets/project/netflix_clone.jpg",
-    github: "https://github.com/SOURABH358/netflix-clone",
-    live: "https://animated-gaufre-45ae75.netlify.app/"
-},
-]
